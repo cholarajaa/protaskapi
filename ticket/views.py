7035153de173b73b5611417f2419c3f87d73035b
cholaraja <madhu.ch@tribe.host>
2018-09-21 14:34:04 +0530

    Simple modelViewset

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from . import serializers
+from . import models
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = models.Ticket.objects.all()
+    serializer_class = serializers.TicketSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = models.Tag.objects.all()
+    serializer_class = serializers.TagSerializer
