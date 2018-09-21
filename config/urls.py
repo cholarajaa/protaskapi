dd005d1e3815cd9713ab8dd38a386c3200edd2f1
cholaraja <madhu.ch@tribe.host>
2018-09-21 14:35:02 +0530

    configure api endpoints

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework import routers
+from ticket import views
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'api/users', views.UserViewSet)
+
+router.register(r'api/tickets', views.TicketViewSet)
+
+router.register(r'api/tags', views.TagViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'', include(router.urls)),
+    path(r'api/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
